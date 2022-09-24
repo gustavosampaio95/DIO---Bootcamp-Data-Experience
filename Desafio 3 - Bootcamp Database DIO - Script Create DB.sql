0c3bff125a0154f4d52342426f0631094c2b9ccb
gustavosampaio <105075069+gustavosampaio95@users.noreply.github.com>
2022-09-24 10:14:14 -0300

    Add files via upload

---
+++ Desafio 3 - Bootcamp Database DIO - Script Create DB.sql
@@ -0,0 +1,92 @@
+-- Criando nosso BD --
+create database ecommerce;
+use ecommerce;
+
+-- Criar tabela cliente
+create table Clients(
+	idClient int auto_increment primary key,
+	Idt int unique,
+    Fname char(60),
+    Adress varchar(100),
+    CPF char(11)
+    );
+    
+-- Criar tabela produto
+create table Product(
+	idProduct int auto_increment primary key,
+    Fname varchar (10) not null,
+	Category enum ('Eletrônico', 'Vestuário', 'Brinquedo', 'Esporte', 'Limpeza') not null,
+    Description char(60),
+	buyValue float,
+    sellValue float,
+    rating float default 0,
+    idPayment int
+    );
+    
+-- Criar tabela pedido
+create table Orders(
+	idOrders int auto_increment primary key,
+    statusOrder enum ('Entregue', 'Separação', 'Enviado') not null,
+    payment_idPayment int,
+    Client_idClient int,
+    shippingValue float,
+    paymentCash bool default false
+    );
+    
+-- Criar tabela Pagamento
+create table payment(
+	idPayment int,
+    idClient int,
+    typePay enum ('credit', 'debit', 'pix'),
+    dateValid date,
+    primary key (idClient, idPayment)
+    );
+    
+-- Query criada para adicionar uma coluna com valor total
+    alter table payment
+		add totalValue float;
+    
+-- Criar tabela estoque
+create table Storage(
+	idProdStorage int auto_increment primary key,
+	storageLocation varchar (100),
+    quantity int default 0
+    );
+
+-- Criar tabela fornecedor
+create table supplier(
+	idSupplier int auto_increment primary key,
+	socialName varchar (100) not null,
+    CNPJ char(15) not null,
+    contact char(11) not null,
+    constraint unique_supplier unique (CNPJ)
+    );
+    
+-- Criar tabela vendedor
+create table partner(
+	idPartner int auto_increment primary key,
+	socialName varchar (100) not null,
+    abstName char (50),
+    CNPJ char(15),
+    CPF char (11),
+    contact char(11) not null,
+    location varchar (150) not null,
+    constraint unique_partner unique (CNPJ),
+    constraint uniquecpf unique (CPF)
+    );
+
+-- Criar tabela produto/parceiro
+create table partSeller(
+	idpartSeller int,
+    idProduct int,
+    quantity int default 1,
+    primary key (idpartSeller, idProduct)
+    );
+    
+-- Criar tabela status
+create table statusorder(
+	idstatusorder int not null primary key,
+    idOrders int not null,
+    shippingcod varchar(16));
+	
+    
---
+++ Desafio 3 - Bootcamp Database DIO - Script Querys.sql
@@ -0,0 +1,20 @@
+-- Usando select para retorno de dados dos clientes
+select * from client
+
+-- Usando Where Statment - Para visualização do modo de pagamento de cada cliente e nome, podemos utilizar:
+select	Fname, typePay
+	from	clients c, payment p
+	where	c.idClient = p.idClient;
+    
+-- Ordendando os dados de Status dos Pedidos pelo preço de frete com Order By:
+select o.statusOrder, o.shippingValue, p.Fname
+	from	orders o, product p, payment pp
+	where	typePay = 'credit' order by o.shippingValue
+    
+-- Ordenando pedidos de produtos eletrônicos que são maiores do que R$100, através do HAVING:
+select o.statusOrder, pp.totalValue, c.Fname, p.Category
+	from orders o, payment pp, clients c, product p
+	where p.Category = 'Eletrônicos'
+		group by p.Category having (totalValue) > 100;
+    
+	
